0caf30cbf63fe9675354a45c01f2fafd7b58e2a0
nascimentorapha <nascimento.rapha@hotmail.com>
2020-06-17 15:33:58 -0300

    included static folder and index page on server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const server = express()
 
+//public folder
+server.use(express.static("public"))
+
 
 server.get("/", (req, res) => {
-    
+    res.sendFile(__dirname + "/views/index.html")
 })
 
 
