f92d52d45639fa424bb24e5ebdb22a89e4accc10
nascimentorapha <nascimento.rapha@hotmail.com>
2020-06-17 16:43:35 -0300

    included nunjucks parameter

--- src/server.js
+++ src/server.js
@@ -4,13 +4,18 @@ const server = express()
 //public folder
 server.use(express.static("public"))
 
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", { //express use the Template Engine
+    express: server,            // and path to render
+    noCache: true
+})
 
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("views/index.html")
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.sendFile("create-point.html")
 })
 
 
